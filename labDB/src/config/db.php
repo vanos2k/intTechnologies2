18f480adaad021ac33842879db09e38c10e7d52a
vanos2k <49749119+vanos2k@users.noreply.github.com>
2021-06-18 09:20:10 +0300

    M

--- labDB/src/config/db.php
+++ labDB/src/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=default',
-    'username' => 'mezinn',
-    'password' => 'iostream',
+    'username' => 'userbane',
+    'password' => 'password',
     'charset' => 'utf8',
 ];
